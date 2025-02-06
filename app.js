ab27591889603512ed28620f023126ec0ff4ea31
Fabricio Lima <fabriciodacostalima@gmail.com>
2025-02-06 18:06:07 -0300

    alterando o limite

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Exibir texto na tela
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirTextoInicial(tag, texto) {
     exibirTextoNaTela('h1', 'Jogo do número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirTextoInicial();
